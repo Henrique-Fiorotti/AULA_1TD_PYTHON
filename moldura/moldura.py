eed13d3d8ab8b0bdb992a05cf8ecbc10ee9767b4
HENRIQUE BERDOLDI FIOROTTI <25170911@educ123.sp.senai.br>
2025-04-29 14:56:54 -0300

    Moldura v1.1

--- moldura/moldura.py
+++ moldura/moldura.py
@@ -1,2 +1,9 @@
 txt = input("Digite algo para fazer a moldura: \n")
-print(f"<-------{txt}------->")
+ch = '\u2588'
+
+for i in range(len(txt) + 4):
+    print(ch, end= '')
+print(f'\n{ch} {txt} {ch}')
+for i in range(len(txt) ):
+    print(ch, end='')
+
